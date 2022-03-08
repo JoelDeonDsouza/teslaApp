f71fc1454615737c2f4c06ed3586543f2a9baf36
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-08 01:27:30 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.contain}>
+        <ImageBackground
+          source={require("./assets/images/ModelS.jpeg")}
+          style={styles.img}
+        />
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subTitle}>Staring at €69,000</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contain: {
+    width: "100%",
+    height: "100%",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+  titleContainer: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "600",
+  },
+  subTitle: {
+    fontSize: 16,
+    color: "#5c5e62",
   },
 });
